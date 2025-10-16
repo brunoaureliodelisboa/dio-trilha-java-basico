f4a4e339f7a592b36f4805ecdfbb9e88db180f9e
Bruno Lisboa <brunoaureliodelisboa@gmail.com>
2025-10-16 05:19:37 -0300

    Documentação e exemplo Calculadora

---
+++ Sintaxe/Documentação/src/Calculadora.java
@@ -0,0 +1,37 @@
+package Sintaxe.Documentação.src;
+
+/**
+* <h1>333</h1>
+* A Calculadora realiza operações matemáticas entre números inteiros
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Gleyson Sampaio
+* @version 1.0
+* @since   01/01/2022
+*/
+public class Calculadora {
+    /**
+   * Este método é utilizado para somar dois números inteiros
+   * @param numeroUm este é o primeiro parâmetro do método
+   * @param numeroDois este é o segundo parâmetro do método
+   * @return int o resultado deste método é a soma dos dois números.
+   * @return2 343434342
+   */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
+
+    /** 
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+    public class teste1 {
+
+    }
+
+}
